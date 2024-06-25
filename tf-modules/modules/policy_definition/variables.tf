996a22359a093a97821ade173f524d5e0ce3c693
Marco Oliva <oliva.marco00@gmail.com>
2024-06-25 16:49:10 +0200

    update

--- tf-modules/modules/policy_definition/variables.tf
+++ tf-modules/modules/policy_definition/variables.tf
@@ -1,23 +1,30 @@
 variable "target_resource_name" {
+  type = string
   description = "Name of the target resource to create the policy name (policy-<target_resource_name>-<policy_name>)"
 }
 
 variable "resource_id" {
+  type = string
   description = " (Required) The ID of the Resource (or Resource Scope) where this should be applied."
 }
 
 variable "parameters_policy" {
+  type = any
   description = "Parameters to apply the policy"
 }
 
 variable "enabled" {
+  type = bool
   description = "parameter used to enable (true) or disable (false) the policy"
+  default = true
 }
 
 variable "policy_name" {
+  type = string
   description = "The name of the policy this must be unique"
 }
 
 variable "display_name" {
+  type = string
   description = "The display name of the azure policy"
 }
